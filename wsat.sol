5bb3dde5862861fe40afe3286dc5fe6271bbb8db
Matthew Kragness <50250493+mkragness@users.noreply.github.com>
2021-06-05 21:37:03 -0700

    Update wsat.sol

--- wsat.sol
+++ wsat.sol
@@ -1,8 +1,8 @@
-// contracts/GLDToken.sol
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ERC20.sol";
 
 contract WrappedSatoshi is ERC20 {
     
